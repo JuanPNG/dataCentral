c0e03724bbf6c6a226857aaf1a0ab83a4282e3a6
Juan Pablo Narvaez Gomez <juann@ebi.ac.uk>
2024-10-24 16:04:59 +0100

    Updating etl and requirements.

--- etl_annotations_gbif.py
+++ etl_annotations_gbif.py
@@ -1,4 +1,5 @@
 from src.annotations_utils import *
+from src.gbif_utils import *
 from src.names_utils import complement_taxonomy_gbif_id
 
 
@@ -9,6 +10,8 @@ def extract_annotations(url, path):
     get_annotation_taxonomy_ena(path=path)
 
     complement_taxonomy_gbif_id(path=path)
+
+    get_occurrences_gbif(path=path, limit=100)
 
 
 
