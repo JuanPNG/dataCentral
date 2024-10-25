a1ed327f0eae1763abd267663d1f578c940efa99
Juan Pablo Narvaez Gomez <juann@ebi.ac.uk>
2024-10-25 09:19:25 +0100

    Adding docstrings.

--- src/data_manipulation_utils.py
+++ src/data_manipulation_utils.py
@@ -3,6 +3,13 @@ import pandas as pd
 
 
 def transform_jsonl_to_pandas(path, occ_file):
+    """
+    Transforms jsonl file produced by get_occurrences_gbif into a pandas dataframe.
+    :param path: Path to the root directory containing occurrences/raw/occ_sp_name.jsonl file.
+    :param occ_file: occ_sp_name.jsonl with occurrence records from GBIF.
+    :return: pandas data frame
+    >>> # transform_jsonl_to_pandas(path='./out', occ_file=occ_Abrostola_tripartita.jsonl)
+    """
     with open(f'{path}/occurrences/raw/{occ_file}', 'r') as f:
         list_of_records = []
 
