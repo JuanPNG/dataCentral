6383d63d5e85904501a42e0f2044870fb7fb2c9a
Juan Pablo Narvaez Gomez <juann@ebi.ac.uk>
2024-11-18 17:28:45 +0000

    Function to extract climate data from xy coordinates.

--- src/data_manipulation_utils.py
+++ src/data_manipulation_utils.py
@@ -1,5 +1,8 @@
 import json
+import os
+
 import pandas as pd
+import rasterio
 
 
 def transform_jsonl_to_pandas(path, occ_file):
@@ -20,3 +23,42 @@ def transform_jsonl_to_pandas(path, occ_file):
         records = pd.json_normalize(list_of_records)
 
         return records
+
+
+def xy_climate_extraction_batch(path_occ_data_file, path_climate_layer_dir):
+    with open(path_occ_data_file, 'r') as f:
+
+        list_of_records = []
+
+        for line in f:
+            record = json.loads(line)
+            data_to_return = dict()
+            data_to_return['accession'] = record['accession']
+            data_to_return['species'] = record['species']
+            data_to_return['decimalLongitude'] = record['decimalLongitude']
+            data_to_return['decimalLatitude'] = record['decimalLatitude']
+            list_of_records.append(data_to_return)
+
+        data_df = pd.json_normalize(list_of_records)
+
+        for file in os.listdir(path_climate_layer_dir):
+            if file.endswith(".tif"):
+                print(f'Extracting {file.rsplit("_")[1]}')
+
+                with rasterio.open(f'{path_climate_layer_dir}{file}') as cli_var:
+                    # Extraction produce a generator of arrays. Unpacked using list.
+                    extracted_vals = list(
+                        cli_var.sample(
+                            xy=[(data_df['decimalLongitude'], data_df['decimalLatitude'])]
+                        )
+                    )
+                    # Getting values from array
+                    data_df[file.rsplit("_")[1]] = [val.item() for val in extracted_vals]
+
+                print(f'Extraction for {file.rsplit("_")[1]} completed.')
+
+        with open(f'{path_climate_layer_dir}climate_dataset.jsonl', 'w') as jsonl_file:
+            for r in range(0, data_df.shape[0]):
+                row_dict = data_df.iloc[r].to_dict()
+                jsonl_file.write(f'{json.dumps(row_dict)}\n')
+            print(f'Climate data extraction file climate_dataset.jsonl saved to {path_climate_layer_dir}')
